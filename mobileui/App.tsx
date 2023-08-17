5bdf4d76eb5f21076bbcab3a4e2e32e72501ce17
Daniel Lindsey <daniellindsey85254@gmail.com>
2023-08-17 15:13:45 -0700

    basin interface, call API

--- mobileui/App.tsx
+++ mobileui/App.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
-import { SafeAreaView, StatusBar, ScrollView, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {
+  SafeAreaView,
+  StatusBar,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+} from 'react-native'
+
+const callApi = async (item: string, state: string) => {
+  const response = await fetch(
+    `https://dlinds.ngrok.dev/CanIRecycle?state=${state}&item=${item}`
+  )
+  const json = await response
+    .json()
+    .then(res => {
+      return res
+    })
+    .catch(err => {
+      console.log({ err })
+    })
+
+  return json
+}
 
 function App(): JSX.Element {
+  const [toDisplay, setToDisplay] = useState<any>()
+
+  const makeCall = () => {
+    console.log('calling')
+    callApi(item, usState)
+      .then(res => {
+        console.log({ res })
+        setToDisplay(res.isRecyclable)
+      })
+      .catch(err => {
+        console.log({ err })
+      })
+  }
+
+  const [usState, setUsState] = useState('')
+  const [item, setItem] = useState('')
+
   return (
     <SafeAreaView style={styles.backgroundStyle}>
       <StatusBar
@@ -11,7 +53,20 @@ function App(): JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={styles.backgroundStyle}
-      ></ScrollView>
+        contentContainerStyle={styles.contentContainer}
+      >
+        <Text style={styles.sectionTitle}>What state are you in?</Text>
+        <TextInput style={styles.input} onChangeText={setUsState} />
+        <Text style={styles.sectionTitle}>
+          What item do yo want to recycle?
+        </Text>
+        <TextInput style={styles.input} onChangeText={setItem} />
+        <Pressable style={styles.submitButton} onPressIn={() => makeCall()}>
+          <Text style={{ color: styles.sectionTitle.color }}>Check!</Text>
+        </Pressable>
+
+        <Text>{toDisplay === true ? 'Yes!' : 'No!'}</Text>
+      </ScrollView>
     </SafeAreaView>
   )
 }
@@ -21,21 +76,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#333',
     flex: 1,
   },
-  sectionContainer: {
-    marginTop: 32,
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    marginBottom: 32,
+    marginTop: 8,
     paddingHorizontal: 24,
+    backgroundColor: 'lightgrey',
+    height: 48,
+    width: '90%',
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    color: '#fff',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  submitButton: {
+    zIndex: 10,
+    backgroundColor: '#2f5a9e',
+    borderRadius: 24,
+    elevation: 4,
+    height: 48,
+    width: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
 
