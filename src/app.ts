e198687234a3fc0ff2d91290ad0c5d2bafc36518
Daniel Lindsey <daniellindsey85254@gmail.com>
2023-08-17 09:30:02 -0700

    add nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express"
+import { test } from "./test"
 const app = express()
 const port = 3001
 
 app.get("/", (req, res) => {
+  console.log("GET /")
+  res.send("Hello World!")
+})
+
+app.get("/test", (req, res) => {
+  console.log(test())
   res.send("Hello World!")
 })
 
